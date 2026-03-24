3ddf785f996e0b4c62df077da420aa358dda37be
Chinmayananda Dhupad <chinmaydhupad@gmail.com>
2026-03-24 09:19:46 +0530

    demo6.c and gdb log files for demo6 and demo7 is done

--- level1/demo6.c
+++ level1/demo6.c
@@ -8,11 +8,65 @@
 // void delete_array(int **arr);
 
 #include <stdio.h>
-int* create_array(int n); 
-void initialize_array(int *arr, int n); 
-void print_array(int *arr, int n); 
+#include <stdlib.h>
+
+int* create_array(int n);
+void initialize_array(int *arr, int n);
+void print_array(int *arr, int n);
 void delete_array(int **arr);
+
 int main() {
-    
+    int n;
+    int *arr;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    if(n <= 0) {
+        printf("Invalid array size!");
+        return 0;
+    }
+
+    arr = create_array(n);
+
+    if(arr == NULL) {
+        printf("Memory allocation failed!");
+        return 0;
+    }
+
+    printf("Enter %d integers: \n", n);
+    initialize_array(arr, n);
+
+    printf("Array elements are: \n");
+    print_array(arr, n);
+
+    delete_array(&arr);
+
+    if(arr == NULL)
+        printf("\nMemory successfully freed and pointer set to NULL.");
+
     return 0;
+}
+
+int* create_array(int n) {
+    return (int*) malloc (n * sizeof(int));
+}
+
+void initialize_array(int *arr, int n) {
+    for (int i=0; i<n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    printf("\n");
+}
+
+void print_array(int *arr, int n) {
+    for(int i=0; i<n; i++){
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+void delete_array(int **arr) {
+    free(*arr);
+    *arr = NULL;
 }
